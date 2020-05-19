56dd3e4ee0723bdea89b94a5019a5e0ecacac1d6
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-05-19 08:10:48 +0000

    create Controller(5/19)

---
+++ app/controllers/shop_comments_controller.rb
@@ -0,0 +1,31 @@
+class ShopCommentsController < ApplicationController
+	
+	before_action :authenticate_user!
+
+	def create
+		shop = Shop.find(params[:shop_id])
+		shop_comment = Shopcomment.new(shop_comment_params)
+		shop_comment.user_id = current_user.id
+		shop_comment.shop_id = shop.id
+		shop_comment.save
+		redirect_back(fallback_location: root_path)
+		# 他の画面に遷移しない
+	end
+
+	def destroy
+		shop = Shop.find(params[:shop_id])
+		shop_comment =Comment.find_by(user_id: current_user.id, shop_id: params[:shop_id])
+		shop_comment.user_id = current_user.id
+		shop_comment.shop_id = shop.id
+		shop_comment.destory
+		redirect_back(fallback_location: root_path)
+		# 他の画面に遷移しない
+	end
+
+	private
+
+	def shop_comment_params
+		params.require(:shop_comment).permit(:comment)
+	end
+
+end
