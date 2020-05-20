18b9f9afc30a4963122e96f925b6e130c1e616fe
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-05-20 05:41:00 +0000

    create routes(5/20)

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
 	devise_for :users
 	root "home#top"
 	get "home/about" => "home#about"
+	resources :user, only: [:show, :edit, :update]
+	resources :shops do
+		resources :likes, only: [:create, :destroy]
+		resources :shop_comments, only: [:create, :destroy]
+	end
   
 end
