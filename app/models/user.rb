ab6acd4f504bb125cb5fbaf52cc038f748b82d41
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-05-20 03:15:12 +0000

    add validates(5/20)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+    validates :name, presence: :true, uniqueness: :true
+    validates :introduction, presence: :true, uniqueness: :true
+    # nameは空白と重複が不可
+    # introductionは空白と重複が不可
+
 	has_many :shops, dependent: :destroy
 	has_many :likes, dependent: :destroy
 	has_many :shop_comments, dependent: :destroy
