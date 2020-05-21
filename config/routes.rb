ef3fdda03f8752ead36ce9cce85d3120dd547760
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-05-21 02:11:57 +0000

    create goodbye,bye action(5/21)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
 
 	devise_for :users
+
 	root "home#top"
+
 	get "home/about" => "home#about"
-	resources :user, only: [:show, :edit, :update]
+
+	resources :users, only: [:show, :edit, :update] do
+		member do
+			get "goodbye"
+			patch "bye"
+		end
+	end
+
 	resources :shops do
 		resources :likes, only: [:create, :destroy]
 		resources :shop_comments, only: [:create, :destroy]
