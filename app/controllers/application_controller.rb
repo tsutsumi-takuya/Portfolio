d5f966bf15b5b571d9de360739ff07eb9ffe59b4
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-05-28 01:59:17 +0000

    create name-login(5/28)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
 
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	private
+
+	def configure_permitted_parameters
+	  devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+	end
+
 	def after_sign_up_path_for(resource)
 		shops_path
 	end
