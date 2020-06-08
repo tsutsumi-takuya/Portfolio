2fe5dd86b3621ecbbf14d504793cce0ea80d17fa
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-06-08 02:18:17 +0000

    edit enum(6/8)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,6 +22,24 @@ $(document).on("ready turbolinks:load", function() {
         fade: true,
         autoplay: true,
         infinite: true,
-        autoplaySpeed: 3500,
+        autoplaySpeed: 3000,
+		responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
     });
 });
