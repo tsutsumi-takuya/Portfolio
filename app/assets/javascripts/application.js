fa0bc42396174004aec32212cd95a5c94b8a2503
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-06-09 11:46:12 +0000

    edit all(6/9)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,30 +17,30 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-$(document).on("ready turbolinks:load", function() {
-    $('.top-photo').slick({
-        fade: true,
-        autoplay: true,
-        infinite: true,
-        autoplaySpeed: 3000,
-		responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    }
-  ]
+$(document).on("ready turbolinks:load", function(){
+    $('.top-photo').not('.slick-initialized').slick({
+      fade: true,
+      autoplay: true,
+      infinite: true,
+      autoplaySpeed: 3000,
+  		responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }
+      ]
     });
 });
 
